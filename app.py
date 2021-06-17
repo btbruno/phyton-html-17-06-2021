2569792c3f7f8f14b29b9b4b0309fc21b7a28d34
btbruno <brunotech16@gmail.com>
2021-06-17 19:13:32 +0000

    Alteração no começo

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello Word<br /> <strong>Agora a coisa vai.</strong> hehe"
+    return "Hello Word<br /> <strong>Agora a coisa vai.</strong> hehe."
